b13c6f019215b3b04ab2953c981463c19a1cf76c
Dima Alhovik <halapsikon@mail.ru>
2019-10-29 09:25:22 +0300

    adeed forgoton

--- by/epam/financer/controller/command/Command.java
+++ by/epam/financer/controller/command/Command.java
@@ -10,6 +10,7 @@ public interface Command {
     String LOG_PATH = "src/by/epam/financer/log/log";
 
     //можно ли так во избежание повтора кода?
+    //где открыть хендлер, чтобы ?
     default void writeLog(Throwable e){
         try {
             Handler handler = new FileHandler(LOG_PATH);
@@ -17,6 +18,7 @@ public interface Command {
             logger.addHandler(handler);
             logger.setUseParentHandlers(false);
             logger.log(Level.INFO, e.getMessage(), e);
+            handler.close();
         } catch (IOException e1) {
         }
     }
